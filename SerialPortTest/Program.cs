05218eb5efaafd3d1f3574ac36b935307d9fe382
Darshaka Pathirana <dpat@syn-net.org>
2022-11-07 03:44:00 +0100

    Implement CalculateChecksum + StartToTargetValue
    StartToTargetValue() asks the user for the start value, the end value
    and the so called 'target time interval', the time  which should pass
    between start and end value.
    
    The message interval is (currently hardcoded) 50ms and the message value
    is then sent to the c172hc-master in corresponding steps via the serial
    interface.
    
    Since the message is generated dynamically, the checksum must be
    recalculated for each value. This is done in the CalculateChecksum
    function.
    
    This code was used for our first tests in the early morning of
    2022-11-07. We noticed problems with the checksum calculation code
    (either on the this (c172hc-fsconnect) or the c172hc-fsmaster side), but
    ran out of time for further investigation.

--- SerialPortTest/Program.cs
+++ SerialPortTest/Program.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.IO.Ports;
+using System.Linq;
 using System.Threading;
 
 public class PortChat
@@ -110,6 +111,16 @@ public class PortChat
         _serialPort.Close();
     }
 
+    private static byte CalculateChecksum(byte[] msg)
+    {
+        byte checksum = 0xFF;
+        for (int i = 0; i < msg.Length; i++)
+        {
+            checksum ^= msg[i]; // XOR
+        }
+        return checksum;
+    }
+
     private static void StartToTargetValue()
     {
         int start_value = 0;
@@ -117,9 +128,76 @@ public class PortChat
         int target_time_interval = 0;
         float message_interval = 0.05F;  // 50ms
         string message;
-        Byte[] message_airspeed_demo = { 0x02, 0x20, 0x00, 0x00, 0x1D, 0x80, 0x42, 0x03 };
-        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+       
 
+        start_value = ReadStartValue();
+        target_value = ReadTargetValue(start_value);
+        target_time_interval = ReadTargetTimeInterval();
+
+        _serialPort.Open();
+        _continue = true;
+        while (_continue)
+        {
+            int step_value = Convert.ToInt32(Math.Ceiling((target_value - start_value) / target_time_interval * message_interval + 0.5));
+            Console.WriteLine("step_value: {0}", step_value);
+
+            if (step_value < 0)
+            {
+                for (int value = start_value; value >= target_value; value += step_value)
+                {
+                    byte[] message_gauge_stx = { 0x02 };
+                    byte[] message_gauge_code = { 0x20 };
+                    byte[] message_value = new byte[5];
+                    Buffer.BlockCopy(message_gauge_code, 0, message_value, 0, 1);
+                    Buffer.BlockCopy(BitConverter.GetBytes(value), 0, message_value, 1, 4);
+                    byte message_checksum = CalculateChecksum(message_value);
+                    byte[] msg = { 0x02, message_value[0], message_value[1], message_value[2], message_value[3], message_value[4], message_value[5], message_checksum, 0x03 };
+
+                    _serialPort.Write(msg, 0, 8);
+                    // Console.WriteLine("value sent:  {0}", value.ToString("X8"));
+                    // https://stackoverflow.com/a/58708490
+                    Console.WriteLine("value sent:  {0}", string.Join(", ", msg.Select(b => "0x" + b.ToString("X2"))));
+                }
+            }
+            else
+            {
+                for (int value = start_value; value <= target_value; value += step_value)
+                {
+                    byte[] message_gauge_stx = { 0x02 };
+                    byte[] message_gauge_code = { 0x20 };
+                    byte[] message_value = new byte[5];
+                    Buffer.BlockCopy(message_gauge_code, 0, message_value, 0, 1);
+                    Buffer.BlockCopy(BitConverter.GetBytes(value), 0, message_value, 1, 4);
+                    byte message_checksum = CalculateChecksum(message_value);
+
+                    byte[] msg = { 0x02, message_value[0], message_value[4], message_value[3], message_value[2], message_value[1], message_checksum, 0x03 };
+                    _serialPort.Write(msg, 0, 8);
+                    // Console.WriteLine("value sent:  {0}", value.ToString("X8"));
+                    // https://stackoverflow.com/a/58708490
+                    Console.WriteLine("value sent:  {0} (checksum: {1})", string.Join(", ", msg.Select(b => "0x" + b.ToString("X2"))), message_checksum.ToString("X2"));
+                }
+            }
+
+            Console.Write("Type QUIT (+ ENTER) to quit or press ENTER to enter new target value: ");
+            message = Console.ReadLine();
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+            if (stringComparer.Equals("quit", message))
+            {
+                _continue = false;
+            }
+            else
+            {
+                start_value = target_value;
+                target_value = ReadTargetValue(start_value);
+            }
+        }
+
+        _serialPort.Close();
+    }
+
+    private static int ReadStartValue()
+    {
+        int _start_value = 0;
         bool valid_input = false;
         string input;
         while (!valid_input)
@@ -129,51 +207,61 @@ public class PortChat
             if (input == "")
             {
                 valid_input = true;
-            } 
+            }
             else
             {
-                valid_input = int.TryParse(input, out start_value);
+                valid_input = int.TryParse(input, out _start_value);
                 if (!valid_input)
                 {
                     Console.WriteLine("Given start value is invalid!");
                 }
             }
         }
-        
-        valid_input = false;
+        return _start_value;
+    }
+    private static int ReadTargetValue(int _start_value)
+    {
+        int _target_value = 0;
+        bool valid_input = false;
+        string input;
+
         while (!valid_input)
         {
             Console.Write("Target value: ");
             input = Console.ReadLine();
-            valid_input = int.TryParse(input, out target_value);
+            valid_input = int.TryParse(input, out _target_value);
             if (!valid_input)
             {
                 Console.WriteLine("Given target value is invalid!");
             }
-            else if (target_value == start_value)
+            else if (_target_value == _start_value)
             {
                 valid_input = false;
                 Console.WriteLine("Start value and target value can not be the same value!");
             }
         }
+        return _target_value;
+    }
 
-        valid_input = false;
+    private static int ReadTargetTimeInterval()
+    {
+        
+        int _target_time_interval = 0;
+        bool valid_input = false;
+        string input;
+
         while (!valid_input)
         {
             Console.Write("Target time interval: ");
             input = Console.ReadLine();
-            valid_input = int.TryParse(input, out target_time_interval);
-            if (target_time_interval < 1)
+            valid_input = int.TryParse(input, out _target_time_interval);
+            if (_target_time_interval < 1)
             {
                 valid_input = false;
                 Console.WriteLine("Target time interval needs to be greater 0!");
             }
         }
-
-        Console.WriteLine("start_value: {0}", start_value);
-        Console.WriteLine("target_value: {0}", target_value);
-        Console.WriteLine("target_time_interval: {0}", target_time_interval);
-        Console.ReadLine();
+        return _target_time_interval;
     }
 
     public static void Read()
